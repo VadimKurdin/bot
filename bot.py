78b4f452ee13e3b1b074164169201f7798cf8e7c
VadimKurdin <vkurdin23@gmail.com>
2025-04-03 13:32:41 +0300

    Новая команда, реагирующая нового пользователя

--- bot.py
+++ bot.py
@@ -6,6 +6,11 @@ bot = telebot.TeleBot(token)
 def start(message):
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
     if "https://" in message.text.lower(): 
